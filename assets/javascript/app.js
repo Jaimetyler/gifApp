03f18329eb5d8913786beb89d772fdd4b97e0c10
Jaime Tyler <jaimetyler88@gmail.com>
2019-03-30 23:37:19 -0400

    got my pictures showing up with the rating tags. I still can't figure out how to get the images to fill in the page. tried floating, dispalying as block, not sure what the deal is but it seems to be working other than that.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,72 @@
-console.log("I'm here, bitch!")
+console.log("I'm here, Bitch!")
 
-//create a variable array for buttons
-var topics = ["shark", "orca", "pufferfish"]
-var searchInput = $("#seaAnimal-input").val().trim();
+$(function(){
+    addButtons(searchArray,"searchButton","#buttons-view");
+    console.log("page loaded");
 
+})
 
-//need to set a variable called type and store it with the content going in the search?
-var queryUrl = 'https://api.giphy.com/v1/gifs/search?q='+searchInput+'&api_key=I1AxUtIfnK3ea4APyhH6EjBSeeMZqzqw'
+var searchArray = ["shark", "orca", "eel"]
 
-//or i think maybe i can use this and still have for things inside the query//
-//var queryUrl = 'https://api.giphy.com/v1/gifs/search?q='+"sea"+"animals"+'&api_key=I1AxUtIfnK3ea4APyhH6EjBSeeMZqzqw'
+function addButtons(searchArray,classToAdd,areaToAdd){
+    $(areaToAdd).empty();
+    for(var i = 0; i < searchArray.length; i++){
+        var a = $("<button>");
+        a.addClass(classToAdd);
+        a.attr('data-type',searchArray[i]);
+        a.text(searchArray[i]);
+        $(areaToAdd).append(a);
 
-function showButtons(){
-    for(i = 0; i < topics.length; i++){
-        var startButtons = $("<button>" + topics[i] + "</button>");
-    $("#buttons").append(startButtons);
     }
 }
-showButtons();
 
-$("#buttons").on("click", function(){
-    console.log("click event working");
-   // var newButtons = $("#buttons").val().trim();
-   // console.log("new button " + newButtons)
+$(document).on("click", ".searchButton", function(){
+    var type = $(this).data('type');
+    var queryURL =" https://api.giphy.com/v1/gifs/search?q="+type+"&api_key=I1AxUtIfnK3ea4APyhH6EjBSeeMZqzqw&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log(response);
+
+        for (var i = 0; i < response.data.length; i++){
+            var searchDiv = $('<div class = "search-item">');
+            var rating = response.data[i].rating;
+            var p = $("<p>").text("Rating : " + rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $("<img>");
+            image.attr('src',still);
+            image.attr('data-still', still);
+            image.attr('data-animated',animated);
+            image.attr('data-state', 'still');
+            image.addClass("searchImage");
+            searchDiv.prepend(p);
+            searchDiv.prepend(image);
+            $("#gifs").prepend(searchDiv);
+        }
+    })
+})
+
+$(document).on("click", ".searchImage", function(){
+    var state = $(this).attr('data-state');
+    if(state == 'still'){
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+})
+
+$("#add-button").on("click", function(){
+    //this looks for the first version of the input
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    addButtons(searchArray,"searchButton","#buttons-view");
+    //need to return false to prevent from re loading the page automatically
+    return false;
+
+
 })
